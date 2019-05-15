cc16288475c7b00b564f9d674ea43919299580ca
vagrant <vagrant@homestead>
2019-05-15 06:20:33 +0000

    优化优惠券模块

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -17,7 +17,6 @@ class OrderService
     {
         // 如果传入了优惠券，则先检查是否可用
         if ($coupon) {
-            // 但此时我们还没有计算出订单总金额，因此先不校验
             $coupon->checkAvailable($user);
         }
         // 开启一个数据库事务
